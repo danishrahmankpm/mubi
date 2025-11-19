192f9dbc475655ebd4325c7160324951b50b93a0
Danish <danishrahmankpm@gmail.com>
2025-11-19 15:58:38 +0530

    chore:react router dom dependencies added

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -5,10 +5,6 @@ import { useSelector } from "react-redux";
 import type { RootState } from "../state/Store";
 
 
-// Generic, reusable Checkout component styled with Tailwind.
-// Designed to mirror the layout in the provided screenshot but
-// accepts generic props so it can be re-used in different apps
-// (eg. a Mubi-like checkout).
 
 export default function CheckoutPage({  logo = "Mubi",  }) {
   
