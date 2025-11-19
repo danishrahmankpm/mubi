8e2fa8f8c1cb43314b844b91b87a1d881d62a33e
Danish <danishrahmankpm@gmail.com>
2025-11-19 14:44:40 +0530

    feat:remove movie reducer added to cartslice

--- src/state/Store.ts
+++ src/state/Store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieSliceReducer from "./MovieSlice"
+import cartSliceReducer from "./CartSlice"
 
 
 export const store=configureStore({
     reducer:{
-        movie:movieSliceReducer
+        movie:movieSliceReducer,
+        cart:cartSliceReducer
     }
 })
 export type RootState= ReturnType<typeof store.getState>
--- src/types/types.ts
+++ src/types/types.ts
@@ -21,3 +21,7 @@ export interface Movie {
   vote_average: number
   vote_count: number
 }
+export type Cart={
+  cart:Movie[],
+  totalPrice:number
+}
